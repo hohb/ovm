1db3253c760b27f8f151e8846628aac70e5e8d64
Jeroen <jeroen@homeofhappybrands.nl>
2014-06-03 12:31:17 +0200

    initial commit
    part 1

---
+++ html/twitter.php
@@ -0,0 +1,46 @@
+<div class="block twitter">
+	<div class="row">
+		<div class="large-4 small-12 columns">
+			<div class="tw-wrap">
+				<div class="tw-img"><img src="assets/images/icons/tw-image.png" /></div>
+				<div class="tw-content-container">
+					<div class="tw-meta">
+						<h3>Bas Scholte in ‘t Hoff</h3>
+						<span>@volleybas</span>
+					</div>
+				<div class="tw-content">
+					Onduidelijke oude begrafenispolis van @reaal is met bemiddeling van @ovmverzekering toch uitgekeerd. Dank!  #VerzNieuws #deklantcentraal
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="large-4 small-12 columns">
+			<div class="tw-wrap">
+				<div class="tw-img"><img src="assets/images/icons/tw-image.png" /></div>
+				<div class="tw-content-container">
+					<div class="tw-meta">
+						<h3>Bas Scholte in ‘t Hoff</h3>
+						<span>@volleybas</span>
+					</div>
+				<div class="tw-content">
+					Onduidelijke oude begrafenispolis van @reaal is met bemiddeling van @ovmverzekering toch uitgekeerd. Dank!  #VerzNieuws #deklantcentraal
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="large-4 small-12 columns">
+			<div class="tw-wrap">
+				<div class="tw-img"><img src="assets/images/icons/tw-image.png" /></div>
+				<div class="tw-content-container">
+					<div class="tw-meta">
+						<h3>Bas Scholte in ‘t Hoff</h3>
+						<span>@volleybas</span>
+					</div>
+				<div class="tw-content">
+					Onduidelijke oude begrafenispolis van @reaal is met bemiddeling van @ovmverzekering toch uitgekeerd. Dank!  #VerzNieuws #deklantcentraal
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
